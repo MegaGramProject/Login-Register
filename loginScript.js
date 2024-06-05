2c25fb6465dad6543197d60d69becaae46a81e6c
Rishav Ray <risjavram@gmail.com>
2024-06-05 16:53:30 -0500

    added animation, translation, and more

--- loginScript.js
+++ loginScript.js
@@ -16,7 +16,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const appleDevices = document.getElementById("appleDevices");
     const androidDevices = document.getElementById("androidDevices");
     const windowsDevices = document.getElementById("windowsDevices");
-
+    const loginPhotos = document.getElementsByClassName("slide");
+    let slideIdx = 0;
+    const lang = document.getElementById("lang");
+    let currLanguage = "en";
+    const apiUrl = "https://deep-translate1.p.rapidapi.com/language/translate/v2";
+    const data = {"q":"","source":"","target":""};
+    const options = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+        'x-rapidapi-key': '14da2e3b7emsh5cd3496c28a4400p16208cjsn947339fe37a4'
+        },
+        body: null
+    };
 
 
     facebookIcon.onclick = function() {
@@ -31,6 +45,63 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'https://www.apple.com';
     }
 
+    isValidEmail = function (email) {
+        let atIndex = email.indexOf('@');
+        if (atIndex < 1 || email.indexOf('@', atIndex + 1) !== -1) {
+            return false;
+        }
+        let localPart = email.substring(0, atIndex);
+        let domainPart = email.substring(atIndex + 1);
+        if (localPart.length === 0 || localPart.length > 64) {
+            return false;
+        }
+    
+        if (domainPart.length === 0 || domainPart.length > 255) {
+            return false;
+        }
+        let dotIndex = domainPart.indexOf('.');
+        if (dotIndex < 1 || dotIndex === domainPart.length - 1) {
+            return false;
+        }
+        let domainLabels = domainPart.split('.');
+        for (let label of domainLabels) {
+            if (label.length === 0 || label.length > 63) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isValidNumber = function(phoneNumberInput) {
+        if (phoneNumberInput.length !== 10) {
+            return false;
+        }
+    
+        for (let i = 0; i < phoneNumberInput.length; i++) {
+            if (isNaN(phoneNumberInput[i])) {
+                return false;
+            }
+        }
+    
+        return true;
+    }
+
+    usernameIsValid = function(usernameInput) {
+        if (usernameInput.length > 30 || usernameInput.length < 1) {
+            return false;
+        }
+    
+        for (let i = 0; i < usernameInput.length; i++) {
+            let char = usernameInput[i];
+            if (!(char >= 'A' && char <= 'Z') && !(char >= 'a' && char <= 'z') && 
+                !(char >= '0' && char <= '9') && char !== '.' && char!="_") {
+                return false;
+            }
+        }
+    
+        return true;
+    }
+
     numberNameEmail.addEventListener("input", function() {
         if(numberNameEmail.value.length > 0) {
             userContainerInfo.style.display = 'inline-block';
@@ -42,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function() {
             numberNameEmail.style.paddingTop = '6px';
         }
 
-        if (numberNameEmail.value.length > 0 &&  password.value.length > 0) {
+        if ((isValidEmail(numberNameEmail.value) || isValidNumber(numberNameEmail.value) || usernameIsValid(numberNameEmail.value)) &&  password.value.length > 0) {
             loginButton.style.backgroundColor = '#347aeb';
             loginButton.style.cursor = 'pointer';
         }
@@ -64,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function() {
             passwordContainerInfo.style.display = 'none';
             password.style.paddingTop = '6px';
         }
-        if (numberNameEmail.value.length > 0 &&  password.value.length > 0) {
+        if ((isValidEmail(numberNameEmail.value) || isValidNumber(numberNameEmail.value) || usernameIsValid(numberNameEmail.value)) &&  password.value.length > 0) {
             loginButton.style.backgroundColor = '#347aeb';
             loginButton.style.cursor = 'pointer';
         }
@@ -88,7 +159,88 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     setLanguage = function (lang) {
+        newLanguage = "";
+        if (lang==="English"){
+            newLanguage = "en";
+        }
+        else if(lang==="Español") {
+            newLanguage = "es";
+        }
+        else if(lang==="Français") {
+            newLanguage = "de";
+        }
+        else if(lang==="हिंदी") {
+            newLanguage = "hi";
+        }
+        else if(lang==="中国人") {
+            newLanguage = "zh-CN";
+        }
+        else if(lang==="বাংলা"){
+            newLanguage = "bn";
+        }
+        if (currLanguage === newLanguage) {
+            return;
+        }
+        data["source"] = currLanguage;
+        data["target"] = newLanguage;
+
+        const allElements = document.querySelectorAll('*');
         language.innerText = lang;
+        const elementsText = [];
+        allElements.forEach(element => {
+            const text = element.innerText.trim();
+            if (text !== '' && (element.className !=="lang" && element.id!=="language") && (element.tagName.toLowerCase()==="p" || element.tagName.toLowerCase()==="footer" ||
+            element.tagName.toLowerCase()==="input" || element.tagName.toLowerCase()==="button") &&
+            element.className!=="orLine" || element.tagName.toLowerCase()=="a") {
+                for (let node of element.childNodes) {
+                    if (node.nodeType === Node.TEXT_NODE) {
+                        data["q"] = node.textContent;
+                        options.body = JSON.stringify(data);
+                        fetch(apiUrl, options)
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('Network response was not ok');
+                            }
+                            return response.json();
+                        }).then(data => {
+                            node.textContent = data['data']['translations']['translatedText'];
+                        }).catch(error => {
+                            console.error('Error:', error);
+                        });
+                    }
+                }
+            }
+        });
+
+        data["q"] = numberNameEmail.placeholder;
+        options.body = JSON.stringify(data);
+        fetch(apiUrl, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }).then(data => {
+            numberNameEmail.placeholder = data['data']['translations']['translatedText'];
+        }).catch(error => {
+            console.error('Error:', error);
+        });
+
+        data["q"] = password.placeholder;
+        options.body = JSON.stringify(data);
+        fetch(apiUrl, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }).then(data => {
+            password.placeholder = data['data']['translations']['translatedText'];
+        }).catch(error => {
+            console.error('Error:', error);
+        });
+
+        currLanguage = newLanguage;
 
     }
 
@@ -107,6 +259,51 @@ document.addEventListener('DOMContentLoaded', function() {
         windowsDevices.style.display = 'inline-block';
     });
 
+    
+    displayFirstLoginPhoto = function() {
+        loginPhotos[slideIdx].style.display = "inline-block";
+        loginPhotos[slideIdx].style.opacity = "0";
+        
+        let incrementOpacity = function() {
+            if (parseFloat(loginPhotos[slideIdx].style.opacity) < 1) {
+                loginPhotos[slideIdx].style.opacity = (parseFloat(loginPhotos[slideIdx].style.opacity) + 0.01).toString();
+                setTimeout(incrementOpacity, 30);
+            }
+        };
+    
+        incrementOpacity();
+    }
+    
+
+    displayFirstLoginPhoto();
+
+
+    displayLoginPhotos = function() {
+        currentPhoto = loginPhotos[slideIdx];
+        if (slideIdx==3) {
+            slideIdx=0;
+        }
+        else{
+            slideIdx++;
+        }
+        newPhoto = loginPhotos[slideIdx];
+        newPhoto.style.display = "inline";
+        newPhoto.style.opacity = "0";
+        let enableTransition = function() {
+            if (parseFloat(newPhoto.style.opacity) < 1) {
+                newPhoto.style.opacity = (parseFloat(newPhoto.style.opacity) + 0.01).toString();
+                currentPhoto.style.opacity = (parseFloat(currentPhoto.style.opacity) - 0.01).toString();
+                setTimeout(enableTransition, 30);
+            }
+        };
+        enableTransition();
+
+    }
+    
+    setInterval(displayLoginPhotos, 4500);
+
+
+
     document.addEventListener('click', function (event) {
         if (!appleDevices.contains(event.target) && appleDevices.style.display !== 'none') {
             appleDevices.style.display = 'none';
@@ -117,5 +314,6 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!windowsDevices.contains(event.target) && windowsDevices.style.display !== 'none') {
             windowsDevices.style.display = 'none';
         }  });
+
 
 });
