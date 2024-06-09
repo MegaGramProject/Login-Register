424e271897d825ba00f8db087f1e0cb6fe9ea38b
Rishav Ray <risjavram@gmail.com>
2024-06-08 23:56:32 -0500

    set up email + texting of confirmation-code

--- loginRegisterBackend/myapp/urls.py
+++ loginRegisterBackend/myapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("createUser/", views.createUser, name="Create User"),
     path("updateUser/<str:username>", views.updateUser, name="Update User"),
     path("removeUser/<str:username>", views.removeUser, name="Remove User"),
-    path("sendEmail/<str:email>", views.sendEmail, name='Send Email'),
+    path("sendEmail/", views.sendEmail, name='Send Email'),
     path("sendText/<str:number>", views.sendText, name='Send Text')
     
 ]
--- loginRegisterBackend/myapp/views.py
+++ loginRegisterBackend/myapp/views.py
@@ -2,8 +2,13 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
-from django.http import HttpResponse
 from .serializers import UserSerializer
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import ssl
+import random
+
 
 
 @api_view(['POST'])
@@ -41,12 +46,63 @@ def removeUser(request, username):
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET'])
-def sendEmail(request, email):
-    #add code to send email
-    return HttpResponse("send email")
+@api_view(['POST'])
+def sendEmail(request):
+    email = request.data["email"]
+    confirmationCode = random.randint(100000, 999999)
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"{confirmationCode} is your Megagram code"
+    message["From"] = "megagram664@gmail.com"
+    message["To"] = email
+    text =f"""\
+    Hi, Someone tried to sign up for a Megagram account with {email} If it was you, enter this confirmation code in the website:
+    {confirmationCode}
+    """
+    html =f"""\
+    <html>
+    <body>
+    <img src="https://static.vecteezy.com/system/resources/thumbnails/025/067/762/small_2x/4k-beautiful-colorful-abstract-wallpaper-photo.jpg" alt="Megagram" style="height:5%; width: 15%; object-fit: contain; margin-left:745px;">
+    <div style="width: 560px; font-family:Arial; line-height:1.4; font-size:23px; margin-left: 630px;">
+        <p>Hi,</p>
+        <p></p>Someone tried to sign up for a Megagram account with {email} If it was you, enter this confirmation code in the website:</p>
+        <p style="font-weight:bold; font-size: 30px; color:#4b4d4b; text-align: center;">{confirmationCode}</p>
+    </div>
+    <p style="color:gray; margin-top: 75px; text-align: center; width:777px; margin-left: 500px;">This email is for Megagram Account Registration. This message was sent to {email}.</p>
+    </body>
+    </html>
+    """
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
+
+    context = ssl.create_default_context()
+    
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login("megagram664@gmail.com", "daqr zlkq vvil exfi")
+            server.sendmail("megagram664@gmail.com", email, message.as_string())
+            return Response({"confirmationCode": confirmationCode}, status=status.HTTP_201_CREATED)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def sendText(request, number):
-    #add code to send text
-    return HttpResponse("send text")
+    confirmationCode = random.randint(100000, 999999)
+    message =  f"{confirmationCode}\nHi, Someone tried to sign up for a Megagram account with {number} If it was you, enter this confirmation code in the website: {confirmationCode}"
+    to = f"{number}@txt.att.net"
+
+    emailMessage = f"Subject:\nTo:{to}\n{message}"
+
+    context = ssl.create_default_context()
+    
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login("megagram664@gmail.com", "daqr zlkq vvil exfi")
+            server.sendmail("megagram664@gmail.com", f"{number}@txt.att.net", emailMessage)
+            return Response({"confirmationCode": confirmationCode}, status=status.HTTP_201_CREATED)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
